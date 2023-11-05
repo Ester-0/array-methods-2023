46f362c6cee9cd0c1b266c33e3731a25d3e49f7a
Ester-0 <azalea_@hotmail.com>
2023-11-05 22:09:22 +0100

    Ok

--- ex1-sin-objetos.js
+++ ex1-sin-objetos.js
@@ -6,27 +6,53 @@
 
 const numbers = [-5, -3, -1, 1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
 
+
 /**
  * Utiliza la método forEach para mostrar cada número por consola
  */
+
+//PORQUE NO FUNCIONA DE ESTA MANERA??
+//console.log("Todos los numeros: ", numbers.forEach((n) => (n)));
+
+numbers.forEach((n) => { console.log("Todos los numeros: ", n)});
 
 /**
  * Utiliza el método find para encontrar el primer número mayor de 10
  */
 
+console.log("1r num mayor de 10: ",
+    numbers.find((n) => n > 10)
+);
+
 /**
  * Utliza el método some para saber si existe un número mayor de 20
  */
+
+console.log("Existe algun num > 20: ",
+    numbers.some((n) => n > 20)
+);
 
 /**
  * Utiliza el método every para comprobar si todos los números de este array son impares
  */
 
+console.log("Son todos los num impares: ",
+    numbers.every((n) => n % 2 != 0)
+);
+
 /**
  * Utiliza el método filter para obtener un nuevo array con todos los números que son mayores de 3 pero menores de 9
  */
+
+console.log("Que numeson son >3 pero <9: ",
+    numbers.filter((n) => n > 3 && n < 9)
+);
 
 /**
  * Utiliza el método map para obtener otro array donde, para cada posición, ontengamos un -1 si el número que ocupaba aquella posición es negativo, y un +1 si el nuemero es positivo
  * [-1, -1, -1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
  */
+
+console.log("Indicar para cada valor con un 1 si es positivo y con un -1 si es negativo: ",
+numbers.map((n) => Math.sign(n))
+);
--- ex2-sin-objetos.js
+++ ex2-sin-objetos.js
@@ -17,14 +17,28 @@ const students = [
 
 // Utiliza el método forEach para mostrar cada elemento del array por la consola
 
+students.forEach((s) => { console.log("Cada elemento: ", s); });
+
 // Utiliza el método findIndex para encontrar que índica ocupa el string "Maria"
+
+console.log("Que puesto ocupa Maria? ", students.findIndex((s) => s == "Maria"));
 
 // Utiliza el método some para saber si en este array existe la alumna "Katerina"
 
+console.log("Existe Katerina? ", students.some((s) => s == "Katerina"));
+
 // Utiliza el método find para saber el primer nombre que tiene 5 o menos carácteres
+
+console.log("1r nombre con 5 o menos letras: ", students.find((s) => s.length <= 5));
 
 // Utiliza el método every para comprobar si todos los strings de este array tienen 4 o más carácteres
 
+console.log("Todos los nombres tienen 4 o mas letras? ", students.every((s) => s.length >=4));
+
 // Utiliza el método filter para obtener un nuevo array que solo incluya aquellos nombres que contengan la letra i
 
+console.log("Nombres con la letra i: ", students.filter((s) => s.includes("i")));
+
 // Utiliza el método map para obtener un nuevo array donde aparezca solamente la primera inicial de cada nombre
+
+console.log("1r letra de cada nombre: ", students.map((s) => s[0]));
